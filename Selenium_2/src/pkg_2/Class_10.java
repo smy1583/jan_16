45a1dd47dfec94ae57ab50184d2540ea300f4a5f
sheshmani yadav <smy1583@gmail.com>
2018-01-16 15:29:00 +0530

    want to push

--- Selenium_2/src/pkg_2/Class_10.java
+++ Selenium_2/src/pkg_2/Class_10.java
@@ -1,5 +1,23 @@
 package pkg_2;
 
-public class Class_10 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class Class_10 
+{
+	public static void main(String[] args) 
+	{
+	System.setProperty("Webdriver.driver"," .\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com");
+	WebElement a =driver.findElement(By.id("email"));
+	a.sendKeys("sheshyadavet2008@gmail.com");
+	WebElement b =driver.findElement(By.id("pass"));
+	b.sendKeys("yadav123");
+	WebElement c =driver.findElement(By.id("u_0_2"));
+	c.click();
+
+}
 }
